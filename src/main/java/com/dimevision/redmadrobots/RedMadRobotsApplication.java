c040ff2be18cea59c0abc95b04d7a02bd8e8b8e7
dimevision <Fommkaa2304@mail.ru>
2022-09-06 20:29:54 +0300

    Инициализировал проект

---
+++ src/main/java/com/dimevision/redmadrobots/RedMadRobotsApplication.java
@@ -0,0 +1,13 @@
+package com.dimevision.redmadrobots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedMadRobotsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedMadRobotsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dimevision/redmadrobots/RedMadRobotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimevision.redmadrobots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedMadRobotsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
