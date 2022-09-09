896cd42ffb112b980c11bb263e2d2277d7fc81d5
dimevision <Fommkaa2304@mail.ru>
2022-09-09 21:42:29 +0300

    Настроил конфигурацию SpringSecurity и переопределил логику фильтров для аутентификации/авторизации через JWT

---
+++ src/main/java/com/dimevision/redmadrobots/config/security/CustomAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.dimevision.redmadrobots.config.security;
+
+import com.dimevision.redmadrobots.config.redis.JwtTokenService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+/**
+ * @author Dimevision
+ * @version 0.1
+ */
+
+@RequiredArgsConstructor
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenService jwtTokenService;
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(username, password);
+
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException {
+        Map<String, String> responseBody = new HashMap<>();
+        responseBody.put("timestamp", LocalDateTime.now().toString());
+        responseBody.put("error", failed.getMessage());
+        responseBody.put("path", request.getContextPath());
+
+        response.setStatus(401);
+        response.setContentType(APPLICATION_JSON_VALUE);
+
+        new ObjectMapper().writeValue(response.getOutputStream(), responseBody);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException {
+        User user = (User) authentication.getPrincipal();
+        Map<String, Object> tokens = jwtTokenService.generateJwtTokens(user.getUsername());
+
+        response.setContentType(APPLICATION_JSON_VALUE);
+
+        new ObjectMapper().writeValue(response.getOutputStream(), tokens);
+    }
+}
